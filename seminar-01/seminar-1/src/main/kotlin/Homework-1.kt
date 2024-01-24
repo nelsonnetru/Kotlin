51421e21a151b2b6a7cc395d4640a49e5489ee25
Alexander <3911394@gmail.com>
2024-01-24 10:15:14 +0300

    Homework-01 edited

--- seminar-01/seminar-1/src/main/kotlin/Homework-1.kt
+++ seminar-01/seminar-1/src/main/kotlin/Homework-1.kt
@@ -6,51 +6,42 @@
 • add <Имя> phone <Номер телефона>
 • add <Имя> email <Адрес электронной почты>
 После выполнения команды, кроме команды exit, программа ждёт следующую команду.
- */ 
-fun main() {
+ */
+fun main(vararg args: String) {
     var com: String? = ""
-    while (com != "exit") {
+    while (true) {
         print("Введите команду: ")
         com = readlnOrNull()
         val comArr = com?.split(" ")
-        if (comArr?.size!! > 0) {
-            if (comArr[0] == "exit") return
-            if (comArr[0] == "help") help()
-            if (comArr[0] == "add" && comArr.size == 4) {
-                if (addRecord(comArr[2], comArr[3])) {
-                    printResult("${comArr[1]} ${comArr[2]} ${comArr[3]}")
-                } else {
-                    printResult("Ошибка! Не удалось выполнить команду!")
+        if (comArr != null) {
+            when (comArr.firstOrNull()) {
+                "exit" -> return
+                "help" -> help()
+                "add" -> {
+                    if (comArr.size == 4 && addRecord(comArr[2], comArr[3]))
+                        printResult("${comArr[1]} ${comArr[2]} ${comArr[3]}")
+                    else
+                        printResult("Ошибка! Не удалось выполнить команду!")
                 }
-            } else {
-                printResult("Ошибка! Не верный набор параметров!")
+                else -> printResult("Ошибка! Не верный набор параметров!")
             }
         }
     }
 }
 
 fun addRecord(com: String, value: String): Boolean {
-    if (com == "phone") {
-        var phoneToParse: String = value
-        if (value.substring(0, 1) == "+") {
-            phoneToParse = value.substring(1, value.length)
-        }
-        return parsePhone(phoneToParse)
+    return when(com) {
+        "phone" -> parsePhone(value)
+        "email" -> parseMail(value)
+        else -> false
     }
-    if (com == "email") {
-        return parseMail(value)
-    }
-
-    return false
 }
 
-fun parsePhone(phone: String): Boolean = phone.matches(Regex("""\d+"""))
+fun parsePhone(phone: String): Boolean = phone.matches(Regex("""^\+?\d+${'$'}"""))
 
 fun parseMail(mail: String): Boolean = mail.matches(Regex("^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})"))
 
-fun printResult(msg: String) {
-    println(msg)
-}
+fun printResult(msg: String) = println(msg)
 
 fun help() {
     println(
